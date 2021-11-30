3f5e1ac34f58fc3a679722613be406f2fd6a7494
Demetrios Fassois <dimitris.fasois@gmail.com>
2021-11-29 19:49:56 -0500

    Added simple move heuristic again for PlayerAI.

--- PlayerAI.py
+++ PlayerAI.py
@@ -2,12 +2,13 @@ import numpy as np
 import random
 import time
 import sys
-import os 
+import os
 from BaseAI import BaseAI
 from Grid import Grid
+from MoveHeuristics import improved_score
 
 # TO BE IMPLEMENTED
-# 
+#
 class PlayerAI(BaseAI):
 
     def __init__(self) -> None:
@@ -15,12 +16,12 @@ class PlayerAI(BaseAI):
         super().__init__()
         self.pos = None
         self.player_num = None
-    
+
     def getPosition(self):
         return self.pos
 
     def setPosition(self, new_position):
-        self.pos = new_position 
+        self.pos = new_position
 
     def getPlayerNum(self):
         return self.player_num
@@ -28,37 +29,41 @@ class PlayerAI(BaseAI):
     def setPlayerNum(self, num):
         self.player_num = num
 
-    def getMove(self, grid: Grid) -> tuple:
-        """ 
+    def getMove(self, grid: Grid, computerAI: BaseAI) -> tuple:
+        """
         YOUR CODE GOES HERE
 
         The function should return a tuple of (x,y) coordinates to which the player moves.
 
-        It should be the result of the ExpectiMinimax algorithm, maximizing over the Opponent's *Trap* actions, 
+        It should be the result of the ExpectiMinimax algorithm, maximizing over the Opponent's *Trap* actions,
         taking into account the probabilities of them landing in the positions you believe they'd throw to.
 
         Note that you are not required to account for the probabilities of it landing in a different cell.
 
         You may adjust the input variables as you wish (though it is not necessary). Output has to be (x,y) coordinates.
-        
-        """
-        pass
 
-    def getTrap(self, grid : Grid) -> tuple:
-        """ 
+        """
+        new_position = improved_score(self, grid, computerAI)
+        return new_position
+
+    def getTrap(self, grid: Grid) -> tuple:
+        """
         YOUR CODE GOES HERE
 
         The function should return a tuple of (x,y) coordinates to which the player *WANTS* to throw the trap.
-        
-        It should be the result of the ExpectiMinimax algorithm, maximizing over the Opponent's *Move* actions, 
-        taking into account the probabilities of it landing in the positions you want. 
-        
+
+        It should be the result of the ExpectiMinimax algorithm, maximizing over the Opponent's *Move* actions,
+        taking into account the probabilities of it landing in the positions you want.
+
         Note that you are not required to account for the probabilities of it landing in a different cell.
 
         You may adjust the input variables as you wish (though it is not necessary). Output has to be (x,y) coordinates.
-        
-        """
-        pass
-        
 
-    
+        """
+        # find all available cells in the grid
+        available_cells = grid.getAvailableCells()
+
+        # find all available cells
+        trap = random.choice(available_cells) if available_cells else None
+
+        return trap
